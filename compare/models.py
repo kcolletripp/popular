fd3f8dc6aabf49c34e57c529263475eac782c87e
kcolletripp <kcolletripp@gmail.com>
2016-10-11 14:09:00 -0400

    finished tutorial

--- compare/models.py
+++ compare/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-#@python_2_unicode_compatible
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -14,8 +14,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
-#@python_2_unicode_compatible
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
