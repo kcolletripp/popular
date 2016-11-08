8226dac5a3bb4533507404718e72ccab17335198
kcolletripp <kcolletripp@gmail.com>
2016-11-08 14:12:00 -0500

    Fixed wiki search, properly collects views

--- search/utils.py
+++ search/utils.py
@@ -4,7 +4,7 @@ import datetime
 from .models import Target
 #from bs4 import BeautifulSoup
 
-#for other files: 
+#for other files:
 #import utils
 #utils.get_wiki(target)
 
@@ -12,7 +12,7 @@ def get_wiki(target):
     url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/user/" + urllib2.quote(target.target_name) + "/daily/20160101/" + datetime.datetime.today().strftime('%Y%m%d')
     data = json.load(urllib2.urlopen(url))
     total_views = 0
-    
+
     for item in data['items']:
         #print(item['views'])
         total_views += item['views']
